0083ba1d23bf271f59d0d695010c0d6518553b2f
sachin <125rathodsachin@gmail.com>
2022-08-20 15:48:12 +0530

    changed home screen for testing schulte game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:bolt/screens/Onboarding/authentication.dart';
 import 'package:bolt/screens/Onboarding/gettingstarted.dart';
+import 'package:bolt/screens/schulte_table/first_screen.dart';
+import 'package:bolt/screens/schulte_table/game_one.dart';
+import 'package:bolt/screens/schulte_table/schulte_levelup.dart';
 import 'package:bolt/services/google_authentication.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
@@ -16,6 +19,8 @@ void main() async {
 class MyApp extends StatelessWidget {
   final user = FirebaseAuth.instance.currentUser;
 
+   MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -23,9 +28,17 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          appBarTheme: const AppBarTheme(color: Colors.black),
           scaffoldBackgroundColor: CustomColors.backgroundColor,
+          bottomSheetTheme: const BottomSheetThemeData(modalBackgroundColor: Color.fromARGB(255, 30, 29, 29)),
         ),
-        home: const Authentication(),
+        home: const FirstScreen(),
+        // const Authentication(),
+        routes: {
+          GameOne.routeName:(ctx) =>const GameOne(),
+          SchulteLevelUp.routeName:(ctx) => SchulteLevelUp(), 
+        }
+        ,
       ),
     );
   }
