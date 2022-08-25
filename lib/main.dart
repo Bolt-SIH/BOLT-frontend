056d5fa748f6ef0082ef545681bddeb4c9459cf1
sachin <125rathodsachin@gmail.com>
2022-08-25 14:12:53 +0530

    added routes for game screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:bolt/screens/Onboarding/authentication.dart';
 import 'package:bolt/screens/Onboarding/gettingstarted.dart';
-import 'package:bolt/screens/schulte_table/first_screen.dart';
-import 'package:bolt/screens/schulte_table/game_one.dart';
-import 'package:bolt/screens/schulte_table/schulte_levelup.dart';
+import 'package:bolt/screens/games/game_collection.dart';
+import 'package:bolt/screens/games/schulte_table/game_intro_screens/first_screen.dart';
+import 'package:bolt/screens/games/schulte_table/game_intro_screens/second_screen.dart';
+import 'package:bolt/screens/games/schulte_table/game_intro_screens/third_screen.dart';
+import 'package:bolt/screens/games/schulte_table/shulte_level_one.dart';
+import 'package:bolt/screens/games/schulte_table/schulte_levelup.dart';
 import 'package:bolt/services/google_authentication.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
@@ -21,7 +24,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   final user = FirebaseAuth.instance.currentUser;
 
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +35,15 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           appBarTheme: const AppBarTheme(color: Colors.black),
           scaffoldBackgroundColor: CustomColors.backgroundColor,
-
-          textTheme: const TextTheme(
-            bodyText1: TextStyle(color: Colors.white),
-            bodyText2: TextStyle(color: Colors.white),
-          ),
+          bottomSheetTheme: const BottomSheetThemeData(modalBackgroundColor: Color.fromARGB(255, 30, 29, 29)),
         ),
-        home: Authentication(),
+        home: const FirstScreen(),
+        // const Authentication(),
+        routes: {
+          GameOne.routeName:(ctx) =>const GameOne(),
+          SchulteLevelUp.routeName:(ctx) => SchulteLevelUp(), 
+        }
+        ,
       ),
     );
   }
