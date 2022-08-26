d0ae34669712e0bde0a798ce23aa013fa1c8d09e
sachin <125rathodsachin@gmail.com>
2022-08-26 14:43:15 +0530

    removed unwanted import

--- lib/screens/CourseOnBoarding/lesson.dart
+++ lib/screens/CourseOnBoarding/lesson.dart
@@ -1,9 +1,6 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:bolt/file_exported.dart';
 import 'package:bolt/screens/games/game_collection.dart';
 import 'package:bolt/theme/app_styles.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:rive/rive.dart';
 
 import '../Learning_Games/learn_start.dart';
@@ -31,44 +28,42 @@ class _LessonCompletedState extends State<LessonCompleted> {
         ),
         backgroundColor: CustomColors.backgroundColor,
       ),
-      body: Container(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.25,
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Center(
+                child: Text(
+              "Lesson Completed",
+              style: TextStyle(
+                  fontFamily: "baloo",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: CustomColors.brightyellow),
+            )),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Center(
-                  child: Text(
-                "Lesson Completed",
-                style: TextStyle(
-                    fontFamily: "baloo",
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: CustomColors.brightyellow),
-              )),
-            ),
+        ),
+        SizedBox(
+          height: 200,
+          child: RiveAnimation.asset(
+            "assets/animation/birb.riv",
+            controllers: [
+              SimpleAnimation("lookUp"),
+              SimpleAnimation("slowDance"),
+            ],
+            // slowDance , idle , lookUp
+            onInit: (_) => setState(() {}),
           ),
-          SizedBox(
-            height: 200,
-            child: RiveAnimation.asset(
-              "assets/animation/birb.riv",
-              controllers: [
-                SimpleAnimation("lookUp"),
-                SimpleAnimation("slowDance"),
-              ],
-              // slowDance , idle , lookUp
-              onInit: (_) => setState(() {}),
-            ),
-          ),
-          const Spacer(
-            flex: 1,
-          ),
-          button("Completed", const GameCollections(), context)
-        ]),
-      ),
+        ),
+        const Spacer(
+          flex: 1,
+        ),
+        button("Completed", const GameCollections(), context)
+      ]),
     );
   }
 }
